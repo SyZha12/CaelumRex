9314d200eaae0e3c9ec2736658c31db448c16501
SyZha12 <sijmenzhang@gmail.com>
2024-10-07 00:33:42 +0200

    Added comments; functionally nothing changed

--- CaelumRex/caelumrex/CaelumRex.h
+++ CaelumRex/caelumrex/CaelumRex.h
@@ -1,7 +1,16 @@
 #ifndef CAELUMREX_H
 #define CAELUMREX_H
 
+// This header file is primarily used for the application (client)
+// The client application will only be able to use functions from these header files
+
+// ---Header(s) used for other applications using this engine
 #include "Core/Application.h"
+// ----------------------------------------------------------
+
+
+// ---Entry point of the application
 #include "Core/EntryApplication.h"
+//----------------------------------
 
 #endif //CAELUMREX_H
--- CaelumRex/caelumrex/include/Core/EntryApplication.h
+++ CaelumRex/caelumrex/include/Core/EntryApplication.h
@@ -1,10 +1,14 @@
 #ifndef ENTRYAPPLICATION_H
 #define ENTRYAPPLICATION_H
 
+// By using this header; main entry point stays separate from the other applications that use this engine
+// Thus, main starts executing from here
 extern CaelumRex::Application* CaelumRex::CreateApplication();
 
 int main(int argc, char ** argv)
 {
+    // STEP 1: Instantiate the application class
+    // By creating the starting class in the client application and returning a pointer; the engine will take care of starting up the whole system
     auto* application = CaelumRex::CreateApplication();
     application->Run();
     delete application;
